81deff1fb186ae4139c069cc4ffb228ee3a854ab
Wander Tillmann Lopes <wanderlopes659@hotmail.com>
2021-03-10 12:00:09 -0300

    alteracoes de layout

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,11 +2,12 @@
 
 @section('title', 'Criacao de posts')
 @section('content')
-<a>Cadastrar o novo Post</h1>
+    <h1 class="text-center text-3x1 uppercase font-black my-4">Cadastrar o novo Post</h1>
 
+    <div class="w-11/12 p-12 bg-whit sm:w-8/12 mdw-1/2 lg:w-5/12 mx-auto">
+        <form action="{{ route('posts.store') }}" method="post" m enctype="multipart/form-data">
+            @include('admin.posts.form')
+        </form>
+    </div>
 
-<form action="{{ route('posts.store') }}" method="post"m enctype="multipart/form-data">
-   @include('admin.posts.form') 
-</form>
 @endsection
-
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,37 +3,69 @@
 @section('title', 'Listagem de posts')
 
 @section('content')
-    <a href="{{ route('posts.create') }}">Criar novo post</a>
-    <hr>
-    @if (session('message'))
+    <h1 class=" text-center text-3x1 uppercase font-black my-8">listagem dos posts</h1>
+    <div class="flex  flex-col box-border">
         <div>
-            {{ session('message') }}
+            <a class="text-center text-3x1 uppercase font-black my-4 float-left" href="{{ route('posts.create') }}">
+                <button class="py-2 px-4 bg-green-500 text-white rounded-lg">Criar novo
+                    post</button>
+            </a>
         </div>
-    @endif
 
-    <form action="{{ route('posts.search') }}" method="post">
-        @csrf
-        <input type="text" name="search" id="" placeholder="filtrar:">
-        <button type="submit">Filtrar</button>
-    </form>
+        <hr>
+        @if (session('message'))
+            <div>
+                {{ session('message') }}
+            </div>
+        @endif
+        <div>
+            <form action="{{ route('posts.search') }}" method="post">
+                @csrf
+                <input class="rounded-lg py-2 px-4 my-4" type="text" name="search" id="" placeholder="filtrar:">
+                <button class="py-2 px-4 bg-blue-500 text-white rounded-lg" type="submit">Filtrar</button>
+            </form>
+        </div>
+        <div>
+            <table class="table-fixed bg-white ">
+                <thead class="px-2">
+                    <tr>
+                        <th class="w-1/4 text-left">ID</th>
+                        <th class="w-1/4  text-left">Image</th>
+                        <th class="w-1/4 ">Nome</th>
+                        <th class="w-1/4 "></th>
+                    </tr>
+                </thead>
+                <tbody class="px-2">
+                    @foreach ($posts as $post)
+                        <tr class="object-center">
+                            <th class="text-left">{{ $post->id }}</th>
+                            <th class="text-left"> <img src="{{ url("storage/{$post->image}") }}" alt=""
+                                    class="obect-fill h-20  object-center"> </th>
+                            <th class="object-center w-screen">{{ $post->title }}</th>
+                            <th class="text-right">
+                                <a href="{{ route('posts.show', $post->id) }}">
+                                    <button class="py-2 px-4 bg-white rounded border-black">Ver detalhes</button>
+                                </a>
+                                <a href="{{ route('posts.edit', $post->id) }}">
+                                    <button class="py-2 px-4 bg-white rounded border-black"> Editar</button>
+                                </a>
+                            </th>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
-    <h1>Posts</h1>
-    @foreach ($posts as $post)
-        <p>
-            <img src="{{ url("storage/{$post->image}") }}" alt="" style="max-width:100px;">
-            {{ $post->title }} -
-            [
-            <a href="{{ route('posts.show', $post->id) }}">Ver detalhes</a> -
-            <a href="{{ route('posts.edit', $post->id) }}">Editar</a>
-            ]
-        </p>
-    @endforeach
-    <hr>
+        </div>
 
-    @if (isset($filters))
-        {{ $posts->appends($filters)->links() }}
-    @else
-        {{ $posts->links() }}
-    @endif
+        <hr>
+
+        @if (isset($filters))
+            {{ $posts->appends($filters)->links() }}
+        @else
+            {{ $posts->links() }}
+        @endif
+
+    </div>
+
 
 @endsection
